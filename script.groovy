aba2a6cd2bdbc83c552d141d15dc515bee4081c0
Toyosi-Balogun <105265503+Toyosi-Balogun@users.noreply.github.com>
2022-11-04 22:35:37 +0100

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,4 +1,3 @@
-
 def buildJar() {
     echo "building the application..."
     sh 'mvn package'
@@ -7,10 +6,9 @@ def buildJar() {
 def buildImage() {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-                        sh "docker build -t typhoenix/my-app:jma-2.0 ."
-                        sh "echo $PASS | docker login -u $USER --password-stdin"
-                        sh "docker push typhoenix/my-app:jma-2.0"
-                    }
+        sh 'docker build -t typhoenix/my-app:jma-2.0 . '
+        sh "echo $PASS | docker login -u $USER --password-stdin"
+        sh 'docker push typhoenix/my-app:jma-2.0'
     }
 } 
 
